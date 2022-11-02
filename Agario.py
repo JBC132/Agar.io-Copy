aa249f87b939163e58492ba85fe36e2c856e139d
이진영 <jakerhee321@gmail.com>
2022-11-03 00:56:01 +0900

    Variables

--- Agario.py
+++ Agario.py
@@ -5,3 +5,20 @@ import math
 import time
 from pygame.locals import *
 pygame.init()
+
+SCREEN = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
+FPS = 30
+CLOCK = pygame.time.Clock()
+pygame.display.set_caption("Agar.io")
+
+cell_count = 2000
+bot_count = 20
+map_size = 2000
+spawn_size = 25
+bots_mins_size = 25
+bots_max_size = 250
+respawn_cells = True
+respawn_bots = False
+player_color = (255,0,0)
+background_color = (0,0,0)
+text_color = (255,255,255)
